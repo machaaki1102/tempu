b3e5b837810a34734f73d0ff4df3af50df6ea926
github <github>
2023-03-15 06:25:50 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -110,7 +110,7 @@ day = start.day
 #気象庁の県コード及び地域コードを入れる
 #prec_no = 57
 #block_no = 47616
-prec_no,block_no = df_sub.loc[df_sub['県'] == ken, ['prec_no','block_no']].value
+prec_no,block_no = df_sub.loc[df_sub['県'] == ken, ['prec_no','block_no']].values[0]
 #prec_no = df_sub.loc[df_sub['県'] == ken]['prec_no']
 #prec_no]
 #.values[0]
