3b8f95ad9c8f3378e78c95ed0c5b82b2de8b97d6
github <github>
2023-03-15 07:47:30 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -7,23 +7,7 @@ import requests
 import numpy as np
 from bs4 import BeautifulSoup
 from dateutil.relativedelta import relativedelta
-
-#prec_noとblock_no含んだ『県マスター』が出来る
-## データを読み込む
-df = pd.read_csv("kishou.txt", header=None,skiprows=1,sep="\s*,\s*", engine="python")
-df_sub = df.iloc[:,[0,1,2,3,4,10]] 
-df_sub.columns = ["No.", "i", "地点名", "prec_no", "block_no","県"]
-
-#block_noの前に数字を埋めて4桁にする
-df_sub["block_no"] = df_sub["block_no"].astype(str)
-
-for a in range(len(df_sub)):
-    num_str = df_sub["block_no"][a]
-    if len(num_str) == 4 and len(num_str) == 5:
-    #4桁でも5桁でもない場合の処理
-        continue
-    else:
-        df_sub['block_no'][a]= "{:0>4}".format(num_str)
+import base64
 
 #何か月違いか
 def month_difference(date1,date2):
@@ -100,6 +84,23 @@ def df_add(date_new):
     return date_new,year,month
 
 #body　データの日付の入力
+#prec_noとblock_no含んだ『県マスター』が出来る
+## データを読み込む
+df = pd.read_csv("kishou.txt", header=None,skiprows=1,sep="\s*,\s*", engine="python")
+df_sub = df.iloc[:,[0,1,2,3,4,10]] 
+df_sub.columns = ["No.", "i", "地点名", "prec_no", "block_no","県"]
+
+#block_noの前に数字を埋めて4桁にする
+df_sub["block_no"] = df_sub["block_no"].astype(str)
+
+for a in range(len(df_sub)):
+    num_str = df_sub["block_no"][a]
+    if len(num_str) == 4 and len(num_str) == 5:
+    #4桁でも5桁でもない場合の処理
+        continue
+    else:
+        df_sub['block_no'][a]= "{:0>4}".format(num_str)
+
 #st.dataframe(df_sub)
 ken =st.selectbox('県を選んでください',df_sub['県'].unique())
 start = st.date_input("開始日を選択してください", value=datetime.date(2022, 1, 5))
