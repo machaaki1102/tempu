c7b8af143f693caef3c76a68470b7601d9e4df6d
github <github>
2023-03-15 08:38:09 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -156,8 +156,7 @@ if st.button("入力完了,データ表示させる"):
     df = df.drop("index", axis=1)
     df = df.reindex(columns=["日付", "平均気温(℃)","最高気温(℃)","最低気温(℃)","降水量(mm)","日照時間(h)"])
     df
-#ついかｄｄ
-import base64
+    
 if st.button('Download CSV'):
     csv = df.to_csv(index=False).encode('utf-8-sig')
     #b64 = base64.b64encode(csv.encode()).decode()
