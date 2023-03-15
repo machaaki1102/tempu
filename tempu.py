03b3cecd3fffd027214defddcdcdd7d0af6fcdf4
github <github>
2023-03-16 06:43:50 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -183,7 +183,7 @@ df_dif = df_ago[["平均気温(℃)","降水量(mm)","日照時間(h)"]] - df[["
 
 #st.write(df_dif.dtypes)
 
-df_dif = df.concat([df[["日付"]],df_dif],axis=1)
+df_dif = df_dif.merge(df[["日付"]],axis=1)
 
 df_dif
 #st.write(df.dtypes)
