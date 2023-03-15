cbbb804f3a7203a99beb8868ac26a86be9794b00
github <github>
2023-03-16 06:34:49 +0900

    d

--- tempu.py
+++ tempu.py
@@ -179,8 +179,9 @@ df_ago = df_ago.query(f"'{start_ago}' <= 日付 <= '{finish_ago}'")
 df_dif = df_ago[["平均気温(℃)","降水量(mm)","日照時間(h)"]] - df[["平均気温(℃)","降水量(mm)","日照時間(h)"]]
 # - df["平均気温(℃)"]
 
-st.write(type(df[["日付"]]))
+st.write(len(df[["日付"]]))
 
+st.write(len(df_dif))
 df_dif = df.concat([df[["日付"]],df_dif],axis=1)
 df_dif
 #st.write(df.dtypes)
