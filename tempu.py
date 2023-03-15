371ff5e8b4cb97276783bf762ee960bff05fa3e4
github <github>
2023-03-15 17:41:02 +0900

    dｄ

--- tempu.py
+++ tempu.py
@@ -156,7 +156,30 @@ if st.button("入力完了,データ表示させる"):
     df = df.reset_index()
     df = df.drop("index", axis=1)
     df = df.reindex(columns=["日付", "平均気温(℃)","最高気温(℃)","最低気温(℃)","降水量(mm)","日照時間(h)"])
-    df_1 = df
+#追加パーツ
+    import plotly.express as px
+    import plotly.graph_objects as go
+# #"日付", "平均気温(℃)","最高気温(℃)","最低気温(℃)","降水量(mm)","日照時間(h)"]
+    fig = px.bar(df_1, x="日付", y=["平均気温(℃)", "最高気温(℃)", "最低気温(℃)", "降水量(mm)", "日照時間(h)"],
+             title="日付別の気象情報",
+             labels={"value": "値", "variable": "変数", "日付": "日付"},
+             barmode="group",
+             height=600)
+    fig.update_layout(xaxis_title="日付", yaxis_title="値")
+    fig.add_trace(go.Scatter(x=df['日付'], y=df['平均気温(℃)'], name="平均気温(℃)", mode="lines"))
+    fig.add_trace(go.Scatter(x=df['日付'], y=df['最高気温(℃)'], name="最高気温(℃)", mode="lines"))
+    fig.add_trace(go.Scatter(x=df['日付'], y=df['最低気温(℃)'], name="最低気温(℃)", mode="lines"))
+    fig.add_trace(go.Scatter(x=df['日付'], y=df['降水量(mm)'], name="降水量(mm)", mode="lines"))
+    fig.add_trace(go.Scatter(x=df['日付'], y=df['日照時間(h)'], name="日照時間(h)", mode="lines"))
+    
+    #fig.add_trace(px.line(df, x="日付", y="平均気温(℃)", name="平均気温(℃)").data[0])
+    #fig.add_trace(px.line(df, x="日付", y="最高気温(℃)", name="最高気温(℃)").data[0])
+    #fig.add_trace(px.line(df, x="日付", y="最低気温(℃)", name="最低気温(℃)").data[0])
+    #fig.add_trace(px.line(df, x="日付", y="降水量(mm)", name="降水量(mm)").data[0])
+    #fig.add_trace(px.line(df, x="日付", y="日照時間(h)", name="日照時間(h)").data[0])
+    df
+    fig.show()
+
 
 if st.button('Download CSV'):
     csv = df.to_csv(index=False).encode('utf-8-sig')
@@ -168,7 +191,7 @@ if st.button('Download CSV'):
         file_name="sample.csv"
     )
 
-if st.button('グラフ表示'):
+#追加パーツ
     import plotly.express as px
     import plotly.graph_objects as go
 # #"日付", "平均気温(℃)","最高気温(℃)","最低気温(℃)","降水量(mm)","日照時間(h)"]
