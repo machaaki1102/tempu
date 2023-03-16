57ac5bef611143e002de6e8cc83e760c73deb3e2
github <github>
2023-03-16 19:30:39 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -121,8 +121,8 @@ for a in range(len(df_sub)):
         df_sub['block_no'][a]= "{:0>4}".format(num_str)
 
 
-st.set_page_config(layout="wide")
-st.title('栽培期間中の気温/降水量/日射量　(気象庁データ)')
+st.set_page_config(page_title="栽培期間中の気温/降水量/日射量(気象庁データ)",layout="wide")
+#st.title('栽培期間中の気温/降水量/日射量　(気象庁データ)')
 #st.dataframe(df_sub)
 
 # 画面の幅を設定
