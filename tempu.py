2b1e18781d310d792501f781cc395d2a07defb36
github <github>
2023-03-18 07:23:48 +0900

    dd

--- tempu.py
+++ tempu.py
@@ -398,7 +398,7 @@ day = start.day
 st.write(prec_no_2)
 st.write(block_no_2)
 
-url = f'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no={prec_no_2}&block_no={block_no_2}&year={year}&month={month}&day={day}&view='
+url = f'https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no={prec_no_2}&block_no=0{block_no_2}&year={year}&month={month}&day={day}&view='
 st.write(url)
 df_df = total_tem2(year,month,day)
 df_df
